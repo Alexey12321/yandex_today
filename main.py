bee9d5ec9ea7eccaafc76ccd5ad5e2e3a2c42c61
Марк <150333996+TANK-MARK-V@users.noreply.github.com>
2024-02-08 16:47:52 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QMainWindow, QPixmap, uic
-import requests
-import sys
-
-
-class Triple(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('calc.ui', self)
